a93cd9bef1ca140355baff8a48dcc52a2def2455
thinh <thinhlx1993@gmail.com>
2019-09-20 03:53:39 +0700

    update code

--- train.py
+++ train.py
@@ -11,116 +11,117 @@ state_size = window_size*5 + 2
 agent = Agent(state_size)
 data = getStockDataVec(stock_name)
 total_sample = len(data) - 1
-batch_size = 32
+batch_size = 128
 buy_amount = 1
 
 for e in range(episode_count + 1):
-	print("Episode " + str(e) + "/" + str(episode_count))
-	order = {
-		'price': 0,
-		'action': 0,
-		'state': None,
-		'next_state': None,
-		'trading': False
-	}
-	state = getState(data, 0, window_size + 1, order)
+    order = {
+        'price': 0,
+        'action': 0,
+        'state': None,
+        'next_state': None,
+        'trading': False
+    }
+    state = getState(data, 0, window_size + 1, order)
 
-	total_profit = 0
-	budget = 1000
-	equity = 1000
-	margin = 0
+    total_profit = 0
+    budget = 1000
+    equity = 1000
+    margin = 0
 
-	for t in range(total_sample):
-		action = agent.act(state)
-		next_state = getState(data, t + 1, window_size + 1, order)
-		reward = 0
+    for t in range(total_sample):
+        action = agent.act(state)
+        next_state = getState(data, t + 1, window_size + 1, order)
+        reward = 0
+        done = False
+        current_stock_price = data[t][3]
+        if action == 0:
+            if order['trading']:
+                _reversed = 1
+                if order['action'] == 2:  # sell order
+                    _reversed = -1
+                profit = (current_stock_price - order['price']) * buy_amount * _reversed
+                reward = 1 if profit > 0 else -1
+                print("Hold order: " + formatPrice(order['price']) + " => " + formatPrice(current_stock_price) + " | Profit: " + formatPrice(profit))
+            else:
+                reward = -1
 
-		current_stock_price = data[t][3]
-		if action == 0:
-			if order['trading']:
-				_reversed = 1
-				if order['action'] == 2:  # sell order
-					_reversed = -1
-				profit = (current_stock_price - order['price']) * buy_amount * _reversed
-				reward = profit
-				print("Hold order: " + formatPrice(order['price']) + " => " + formatPrice(current_stock_price) + " | Profit: " + formatPrice(profit))
-			else:
-				reward = -1
+        elif action == 1:  # place order buy
+            if order['trading']:
+                reward = -5
+            else:
+                margin += agent.calculate_margin(current_stock_price, buy_amount)
+                order = {
+                    'price': current_stock_price,
+                    'action': action,
+                    'state': state,
+                    'next_state': next_state,
+                    'trading': True
+                }
+                reward = 1
+                print("Buy: " + formatPrice(current_stock_price))
+        elif action == 2:  # place order sell
+            if order['trading']:
+                reward = -5
+            else:
+                margin += agent.calculate_margin(current_stock_price, buy_amount)
+                order = {
+                    'price': current_stock_price,
+                    'action': action,
+                    'state': state,
+                    'next_state': next_state,
+                    'trading': True
+                }
+                reward = 1
+                print("Sell: " + formatPrice(current_stock_price))
 
-		elif action == 1:  # place order buy
-			if order['trading']:
-				reward = -5
-			else:
-				margin += agent.calculate_margin(current_stock_price, buy_amount)
-				order = {
-					'price': current_stock_price,
-					'action': action,
-					'state': state,
-					'next_state': next_state,
-					'trading': True
-				}
-				reward = 1
-				print("Buy: " + formatPrice(current_stock_price))
-		elif action == 2:  # place order sell
-			if order['trading']:
-				reward = -5
-			else:
-				margin += agent.calculate_margin(current_stock_price, buy_amount)
-				order = {
-					'price': current_stock_price,
-					'action': action,
-					'state': state,
-					'next_state': next_state,
-					'trading': True
-				}
-				reward = 1
-				print("Sell: " + formatPrice(current_stock_price))
+        elif action == 3:  # close order
+            if not order['trading']:
+                reward = -5
+            else:
+                _reversed = 1
+                if order['action'] == 2:  # sell order
+                    _reversed = -1
 
-		elif action == 3:  # close order
-			if not order['trading']:
-				reward = -5
-			else:
-				_reversed = 1
-				if order['action'] == 2:  # sell order
-					_reversed = -1
+                profit = (current_stock_price - order['price']) * buy_amount * _reversed
+                reward = 1 if profit > 0 else -1
 
-				profit = (current_stock_price - order['price']) * buy_amount * _reversed
-				reward = profit
+                #if reward < 0:
+                #    agent.memory.append((order['state'], order['action'], reward, order['next_state'], True))
 
-				if reward < 0:
-					agent.memory.append((order['state'], order['action'], reward, order['next_state'], True))
+                budget += profit
+                order = {
+                    'price': 0,
+                    'action': 0,
+                    'state': None,
+                    'next_state': None,
+                    'trading': False
+                }
+                print("Close order: " + formatPrice(current_stock_price) + " | Profit: " + formatPrice(profit))
 
-				budget += profit
-				order = {
-					'price': 0,
-					'action': 0,
-					'state': None,
-					'next_state': None,
-					'trading': False
-				}
-				print("Close order: " + formatPrice(current_stock_price) + " | Profit: " + formatPrice(profit))
+                done = True if (profit < 0) else False
+                
+        agent.memory.append((state, action, reward, next_state, done))
+        state = next_state
 
-		done = True if (budget < 0) else False
-		agent.memory.append((state, action, reward, next_state, done))
-		state = next_state
+        if done:
+            print("--------------------------------")
+            print("Episode " + str(e) + "/" + str(episode_count) + " Timestep: " + str(t))
+            print("Budget: " + formatPrice(budget))
+            print("--------------------------------")
+            order = {
+                'price': 0,
+                'action': 0,
+                'state': None,
+                'next_state': None,
+                'trading': False
+            }
+            #budget = 1000
+            #equity = 1000
+            #margin = 0
 
-		if done:
-			print("--------------------------------")
-			print("Budget: " + formatPrice(budget))
-			print("--------------------------------")
-			order = {
-				'price': 0,
-				'action': 0,
-				'state': None,
-				'next_state': None,
-				'trading': False
-			}
-			budget = 1000
-			equity = 1000
-			margin = 0
+        if len(agent.memory) > batch_size and t % 10 == 0:
+            agent.expReplay(batch_size)
 
-		if len(agent.memory) > batch_size:
-			agent.expReplay(batch_size)
-
-	if e % 10 == 0:
-		agent.actor.save("models/model_ep" + str(e))
+    if e % 10 == 0:
+        agent.actor.save("models/model_ep" + str(e))
